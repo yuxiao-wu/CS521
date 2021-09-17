34a3eac5b4ded94d638fd27301b99eb0b35dcdb9
Yuxiao Wu <yxwu@bu.edu>
2021-09-17 19:22:30 -0400

    1

--- hw1.py
+++ hw1.py
@@ -1,7 +1,8 @@
 # File name: hw1.py
 # Created by: Yuxiao Wu
 # Created on : 9/15/2021
-# no collaborators, no sources, no late days
+# no collaborators, no late days
+# source for formula: https://sciencenotes.org/surface-area-formulas-volume-formulas/
 
 
 # Problem 6
@@ -21,8 +22,8 @@ def bit_or(a, b):
 # input the number of hours since Wed, Jan 1st 2020 at 12am, return a string result
 def calculate_day(num_hours):
     num_day = num_hours / 24  # number of days elapsed
-    hour = num_hours % 24     # hours in the day
-    day = num_day % 7         # days in the week
+    hour = num_hours % 24  # hours in the day
+    day = num_day % 7  # days in the week
     if day == 0:
         day_str = "Wednesday "
     elif day == 1:
@@ -46,6 +47,18 @@ def calculate_day(num_hours):
     return daytime
 
 
-
 # Problem 9
+# calculates the volume of sphere or cone with input radius and height
+def calculate_volume(shape, dimensions):
+    if shape == "sphere":
+        volume = round(4 / 3 * 3.14 * dimensions ** 3, 2)
+        return volume
+    elif shape == "cone":
+        volume = round(1 / 3 * 3.14 * dimensions[1] * dimensions[0] ** 2, 2)
+        return volume
+    return "Invalid Shape"
+
+# end
+a = calculate_volume("con", (3,7))
+print(a)
 
